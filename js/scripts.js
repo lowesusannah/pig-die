59ec26833408f23b8aaa015e149e5c5cac105e6a
Joseph Tomlinson and Susannah Lowe <susannah@>
2017-09-27 11:33:10 -0700

    Add roll functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function Game() {
   this.players = [];
+  this.currentPlayer = 0;
   this.round = 0;
 }
 
@@ -7,6 +8,14 @@ Game.prototype.addPlayer = function(id) {
   var player = new Player(id)
   this.players.push(player)
 };
+
+Game.prototype.initialize = function() {
+
+}
+
+Game.prototype.roll = function() {
+  return parseInt(Math.random() * 6) + 1;
+}
 
 function Player(id) {
   this.id = id;
@@ -17,6 +26,25 @@ function Player(id) {
 }
 
 // JQUERY //
+var initGameDisplay = function(game) {
+  game.players.forEach(function(player) {
+    var id = player.id;
+    player.name = $("#player-" + id + "-name").val();
+    $("#player-" + id + "-form").hide();
+    $("#preround-control").hide();
+    $("#player-" + id + "-title").text(player.name);
+    $("#player-" + id).append(`
+      <p>Score: <span id="player-` + id + `-score"></span>
+    `)
+    updateScores(player);
+  });
+}
+
+var turnDisplayUpdate = function(player) {
+  $("#current-player").text(player.name);
+  $("#roll-total").text(player.softScore);
+}
+
 
 var addPlayer = function(game) {
   var id = game.players.length;
@@ -25,34 +53,60 @@ var addPlayer = function(game) {
     <div id="` + playerid + `" class="well">
       <div class="row">
         <div class="col-md-9">
-          <form id="` + playerid + `-name" class="form-name">
+          <form id="` + playerid + `-form" class="form-name">
             <div class="form-group">
               <input class="form-control" type="text" placeholder="Enter player name" id="` + playerid + `-name">
             </div>
           </form>
-          <h2 id="` + playerid + `-name" class="playername"></h2>
+          <h2 id="` + playerid + `-title" class="playername"></h2>
         </div>
       </div>
     </div>
   `);
+  if (id > 1) {
+    $("#preround-remove").show();
+  }
+
+  if (id >= 9) {
+    $("#preround-add").hide();
+  }
   game.addPlayer(id);
+}
+
+var updateScores = function(player) {
+  $("#player-" + player.id + "-score").text(player.hardScore)
 }
 
 var removePlayer = function(game) {
   var player = game.players.pop();
-  var playerid = "player-"+ player.id;
-  $("#"+playerid).remove();
+  console.log(player);
+  var id = player.id;
+  $("#player-"+id).remove();
+  if (id === 2) {
+    $("#preround-remove").hide();
+  }
+  $("#preround-add").show();
 }
 
 $(document).ready(function() {
   var game = new Game();
 
   addPlayer(game);
+  addPlayer(game);
   $("#preround-add").click(function() {
     addPlayer(game);
   });
   $("#preround-remove").click(function() {
     removePlayer(game);
+  });
+  $("#preround-start").click(function() {
+    initGameDisplay(game);
+  });
+  $("#die-roll").click(function(){
+    var roll = game.roll();
+    $("#roll-result").text("You rolled a " + roll);
+    game.players[0].softScore = game.players[0].softScore + roll;
+    turnDisplayUpdate(game.players[0]);
   });
 
 });
